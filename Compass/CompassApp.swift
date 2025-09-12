d4fdc10e8051a46436ec7e99bf124ab45fe91a12
Spike-36 <peter.milligan@mac.com>
2025-09-12 10:52:26 +0900

    Initial Commit

---
+++ Compass/CompassApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompassApp.swift
+//  Compass
+//
+//  Created by Peter Milligan on 12/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct CompassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CompassTests/CompassTests.swift
@@ -0,0 +1,17 @@
+//
+//  CompassTests.swift
+//  CompassTests
+//
+//  Created by Peter Milligan on 12/09/2025.
+//
+
+import Testing
+@testable import Compass
+
+struct CompassTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
